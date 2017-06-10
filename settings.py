03990bbaf6699a206dd6987cdc3fc512eb9ee012
Paul Aswa <aswapaul@gmail.com>
2017-06-10 10:37:09 +0300

    configure database path

--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'operations.db',
+        'NAME': os.path.join(PROJECT_ROOT, 'operations.db'),
     }
 }
 

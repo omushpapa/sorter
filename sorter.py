cf0c21662d76ebe9062e9d4b5b75d7719e64d78a
Paul Aswa <aswapaul@gmail.com>
2017-06-19 16:09:47 +0300

    Remove django initialisation

--- sorter.py
+++ sorter.py
@@ -5,33 +5,24 @@ import logging
 from gui.tkgui import TkGui
 from operations import SorterOps
 from helpers import DatabaseHelper
+from settings import DATABASES, LOG_FILE
 
-# Django configuration
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-from django.conf import settings
-
-DB_NAME = settings.DATABASES['default']['NAME']
-LOG_FILE = settings.LOG_FILE
-
+DB_NAME = DATABASES['default']['NAME']
 
 if __name__ == '__main__':
-	db_helper = DatabaseHelper(DB_NAME)
-	# Create database and tables
-	db_helper.initialise_db()
+    db_helper = DatabaseHelper(DB_NAME)
+    # Create database and tables
+    db_helper.initialise_db()
 
-	# Initialise operations
-	operations = SorterOps(db_helper)
+    # Initialise operations
+    operations = SorterOps(db_helper)
 
-	# Initialise logger
-	logging.basicConfig(filename=LOG_FILE,
-						format='%(asctime)s %(message)s', level=logging.INFO)
+    # Initialise logger
+    logging.basicConfig(filename=LOG_FILE,
+                        format='%(asctime)s %(message)s', level=logging.INFO)
 
-	# Initialise GUI
-	app = TkGui(operations=operations, logger=logging)
+    # Initialise GUI
+    app = TkGui(operations=operations, logger=logging)
 
-	# Show window
-	app.tk_run()
+    # Show window
+    app.tk_run()
